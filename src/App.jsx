69553860f003db4c4cf4832cb82b132a145d804c
Daniel Hernandez <danielhh.dev@gmail.com>
2022-09-07 20:04:42 -0500

    asynchronous function is added to consult the api and is called in useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,23 @@
 import UsersForm from "./components/UsersForm";
 import UsersContainer from "./components/UsersContainer";
 import { useState } from "react";
+import axios from 'axios';
+import { useEffect } from "react";
 
-const initialDB = [
-  {
-    id: 5,
-    name: "John Doe",
-    email: "john@gmail.com",
-    username: "john",
-  },
-  {
-    id: 3,
-    name: "John Doe",
-    email: "john@gmail.com",
-    username: "john",
-  },
-  {
-    id: 1,
-    name: "Fanny",
-    email: "fanny@gmail.com",
-    username: "gatita",
-  },
-];
 
 const App = () => {
-  const [DB, setDB] = useState(initialDB);
+  const [DB, setDB] = useState([]);
   const [userToEdit, setUserToEdit] = useState(null);
+  
+  const getUsers = async() => {
+    const URL = '';
+    const result = await axios.get(URL);
+    setDB(result);
+  }
+  
+  useEffect(()=>{
+    getUsers();
+  });
 
   const createUser = (user) => {
     user.id = Date.now();
